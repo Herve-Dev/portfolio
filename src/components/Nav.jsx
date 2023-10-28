aba7b1e5cd5338abd621f0181b90a713b37d0ace
Herve-Dev <herve.developper@gmail.com>
2023-10-28 17:19:28 +0200

    work on navlink with style and add new folder utils with mixins

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function Nav() {
+  const setActive = ({ isActive }) =>
+    isActive ? "nav-link nav-active" : "nav-link";
+
   return (
-    <ul>
-      <NavLink to="/">
+    <ul className="block-nav">
+      <NavLink to="/" className={setActive}>
         <li>Home</li>
       </NavLink>
-      <NavLink to="/projects">
+      <NavLink to="/projects" className={setActive}>
         <li>Projets</li>
       </NavLink>
-      <NavLink to="contact">
+      <NavLink to="contact" className={setActive}>
         <li>Contact</li>
       </NavLink>
     </ul>
