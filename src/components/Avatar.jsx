054ff82e977e7c72a96a419d5390ecc692f19154
Herve-Dev <herve.developper@gmail.com>
2023-10-29 18:22:40 +0100

    add avatar png

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import image from "../assets/img/avatar.jpg";
+import image from "../assets/img/avatar.png";
 
 function Avatar() {
   return (
     <article className="block-avatar">
       <article className="first-section-avatar">
         <div>
-          <img src={image} alt="" />
+          <img src={image} alt="photo avatar" />
         </div>
       </article>
-      <article></article>
+      <article className="second-section-avatar">
+        <h2>Herve Taillefond</h2>
+      </article>
     </article>
   );
 }
